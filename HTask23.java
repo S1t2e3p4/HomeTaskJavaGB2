c352b6689b926e0526b6f46bc4e6b21b655ba249
Stepuro Mikhail <s1t2e3p4@mail.ru>
2023-03-03 15:35:47 +0300

    Написал решение задачи 3.

--- HTask23.java
+++ HTask23.java
@@ -1,2 +1,35 @@
+/**
+ * Задача 3:
+ * Дан массив целых чисел. Заменить отрицательные элементы на сумму индексов двузначных элементов массива.
+ */
+import java.util.Scanner;
+import java.util.Arrays;
 public class HTask23 {
+    public static void main(String[] args) {
+        System.out.printf("Массив с заменеными отрицательными элементами на сумму индексов двузначных элементов массива: " + Arrays.toString(replacementOfNegativeElements()));
+    }
+
+    public static int[] replacementOfNegativeElements() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число, равное колличеству элементов в Вашем массиве, и нажмите enter: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        int sumOfTheIndicesOfTwodigitElements = 0;
+        System.out.println("Введите ЦЕЛОЕ число и нажмите enter: ");
+
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+            if (Math.abs(array[i])>=10 && Math.abs(array[i])<=99) {
+                sumOfTheIndicesOfTwodigitElements+=i;
+            }
+        }
+        scanner.close();
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] <0) {
+                array[i]=sumOfTheIndicesOfTwodigitElements;
+            }
+        }
+        return array;
+    }
 }
